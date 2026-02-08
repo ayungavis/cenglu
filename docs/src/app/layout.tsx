1799d1874c475955c04caf173151c5b24a0f60ea
Wahyu Kurniawan <ayungavis@gmail.com>
2026-02-09 01:37:20 +0700

    feat: add umami analytics

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -9,6 +9,13 @@ const inter = Inter({
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html className={inter.className} lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          data-website-id="3ed29d0d-5c04-4d64-969f-ad81e0b8ff0b"
+          defer
+          src="https://cloud.umami.is/script.js"
+        />
+      </head>
       <body className="flex min-h-screen flex-col">
         <RootProvider>{children}</RootProvider>
       </body>
